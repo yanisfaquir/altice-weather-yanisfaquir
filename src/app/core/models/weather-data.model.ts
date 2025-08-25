bbb5731b28547a7db664afb88c460b9713136f66
yanisfaquir <84600836+yanisfaquir@users.noreply.github.com>
2025-08-25 19:15:02 +0100

    refactor: move enums and types for WeatherData to a separate file

--- src/app/core/models/weather-data.model.ts
+++ src/app/core/models/weather-data.model.ts
@@ -1,3 +1,4 @@
+import {TemperatureUnit, NetworkPower, AltitudeUnit} from './weather.enums'
 export interface WeatherData {
   id?: string;
   city: string;
@@ -12,24 +13,3 @@ export interface WeatherData {
   updatedAt?: Date;
 }
 
-export enum TemperatureUnit {
-  CELSIUS = 'celsius',
-  FAHRENHEIT = 'fahrenheit'
-}
-
-export enum AltitudeUnit {
-  METERS = 'meters',
-  FEET = 'feet'
-}
-
-// Network power scale from 1 to 5
-export type NetworkPower = 1 | 2 | 3 | 4 | 5;
-
-// Labels for network power for UI display
-export const NetworkPowerLabels: Record<NetworkPower, string> = {
-  1: 'Very Poor',
-  2: 'Poor',
-  3: 'Fair', 
-  4: 'Good',
-  5: 'Excellent'
-} as const;
