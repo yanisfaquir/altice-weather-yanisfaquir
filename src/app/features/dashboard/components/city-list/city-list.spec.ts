16120f201206a10a7b74931758d2fe4aad956740
yanisfaquir <84600836+yanisfaquir@users.noreply.github.com>
2025-08-25 21:59:04 +0100

    feat: add city details button and modal to CityListComponent

--- src/app/features/dashboard/components/city-list/city-list.spec.ts
+++ src/app/features/dashboard/components/city-list/city-list.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { CityList } from './city-list';
+import { CityListComponent } from './city-list';
 
 describe('CityList', () => {
-  let component: CityList;
-  let fixture: ComponentFixture<CityList>;
+  let component: CityListComponent;
+  let fixture: ComponentFixture<CityListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CityList, HttpClientTestingModule]
+      imports: [CityListComponent, HttpClientTestingModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CityList);
+    fixture = TestBed.createComponent(CityListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
